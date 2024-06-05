814c9b7cc8c1b47aebcf4a81926449c0680df12f
natakurage <uzogamuzol@gmail.com>
2024-06-05 09:25:06 +0900

    texture-test

---
+++ utils/textures.ts
@@ -0,0 +1,47 @@
+import p5Types from 'p5'
+
+class Texture {
+  p5: p5Types
+  constructor(p5: p5Types) {
+    this.p5 = p5
+  }
+
+  sample = (pos: p5Types.Vector) => {
+    throw new Error("not implemented")
+    return this.p5.createVector(0, 0, 0)
+  }
+}
+
+class UniformColorTexture extends Texture {
+  color: p5Types.Vector
+  constructor(p5: p5Types, color: p5Types.Vector) {
+    super(p5)
+    this.color = color
+  }
+
+  sample = (pos: p5Types.Vector) => {
+    return this.color.copy()
+  }
+}
+
+class CheckerTexture extends Texture {
+  color1: p5Types.Vector
+  color2: p5Types.Vector
+  scale: number
+  constructor(p5: p5Types, color1: p5Types.Vector, color2: p5Types.Vector, scale: number) {
+    super(p5)
+    this.color1 = color1
+    this.color2 = color2
+    this.scale = scale
+  }
+
+  sample = (pos: p5Types.Vector) => {
+    const mod = (n: number , d: number) => ((n % d) + d) % d
+    if ((mod(this.scale * pos.x, 1) < 0.5) === (mod(this.scale * pos.y, 1) < 0.5)) {
+      return this.color1.copy()
+    }
+    return this.color2.copy()
+  }
+}
+
+export { Texture, UniformColorTexture }
